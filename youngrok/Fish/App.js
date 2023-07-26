7127e58e5d461b14eb06257259c1259b5a10180c
정영록 <s9285s@naver.com>
2023-07-26 17:23:26 +0900

    7/26 merge

--- youngrok/Fish/App.js
+++ youngrok/Fish/App.js
@@ -5,36 +5,12 @@ import { NavigationContainer, } from '@react-navigation/native';
 import { createNativeStackNavigator, createStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Gps from './Gps';
-import Home from './Home';
-import Dictionary from './Dictionary';
-import Feed from './Feed';
-import Camera from './Camera';
-import ModalTest from './ModalTest';
+
+import Tabs from './navigation/tabs';
+import DogamGallery from './screens/dogam/DogamGallery';
 
 
 const Stack = createNativeStackNavigator();
-
-function RootNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
-    </Stack.Navigator>
-  );
-}
-
-const BottomTab = createBottomTabNavigator();
-function BottomTabNavigator() {
-  return (
-    <BottomTab.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
-      <BottomTab.Screen name="Gps" component={Gps} />
-      <BottomTab.Screen name="ModalTest" component={ModalTest} />
-      <BottomTab.Screen name="Home" component={Home} />
-      <BottomTab.Screen name="Feed" component={Feed} />
-      <BottomTab.Screen name="Dictionary" component={Dictionary} />
-    </BottomTab.Navigator>
-  );
-}
 
 
 export default function App() {
@@ -42,8 +18,8 @@ export default function App() {
   return(
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Root" component={BottomTabNavigator} />
-        <Stack.Screen name="Camera" component={Camera} />
+        <Stack.Screen name="Root" component={Tabs} />
+        <Stack.Screen name="DogamGallery" component={DogamGallery} />
       </Stack.Navigator>
     </NavigationContainer>
   );
