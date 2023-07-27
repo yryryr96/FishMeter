d0aeca7fed46f46bcca569062af4c06bdba77f84
정영록 <s9285s@naver.com>
2023-07-27 17:51:36 +0900

    소셜로그인 구현

---
+++ youngrok/Fish/KaKaoLogOut.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { WebView } from 'react-native-webview';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
+
+export default function KaKaoLogOut() {
+    const navigation = useNavigation();
+    const requestLogout = async () => {
+        console.log("logout")
+        const AccessToken = await AsyncStorage.getItem("userAccessToken")
+        console.log(AccessToken)
+        axios({
+            method : 'post',
+            url : 'https://kapi.kakao.com/v1/user/unlink',
+            headers : {
+                Authorization : `Bearer ${AccessToken}`
+            }
+        }).then((res) => {
+            console.log(res)
+            navigation.navigate("Gps",{screen : "Gps"})
+        }).catch((e) => {
+            console.log(e)
+        })
+        
+    }
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={requestLogout}>
+                <Text style={{fontSize:30}}>카카오 로그아웃</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 24,
+      backgroundColor: '#fff',
+    },    
+  });
