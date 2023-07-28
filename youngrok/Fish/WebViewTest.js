61334cc1d8c52177fd33b7ee2dd1017ce0943b78
정영록 <s9285s@naver.com>
2023-07-28 17:20:25 +0900

    login 화면 테스트

--- youngrok/Fish/WebViewTest.js
+++ youngrok/Fish/WebViewTest.js
@@ -1,19 +1,37 @@
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
-import { Alert } from 'react-native';
-
-const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('CHECK!')`;
-const html = `
-    <div>TEST</div>
-`;
-
+import { Alert, StyleSheet,View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function WebViewTest() {
+  const navigation = useNavigation();
+
   return (
-    <WebView
-      source={{html}}
-      injectedJavaScript={INJECTED_JAVASCRIPT}
-      onMessage={(event)=> Alert.alert(event.nativeEvent.data) }
-    />
-  );
+    <View style={{flex:1, justifyContent : 'center', alignItems :'center'}}>
+      <Text>로그인 화면</Text>
+      <TouchableOpacity 
+        style={{width : "70%", backgroundColor : 'green', alignItems : 'center', borderRadius : 20, margin:20}}
+        onPress={()=>navigation.navigate("KaKaoLogin",{screen:"KaKaoLogin"})}
+        >
+        <Text style={{fontSize:30, color : 'white' }}>카카오 로그인</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity 
+        style={{width : "70%", backgroundColor : 'green', alignItems : 'center', borderRadius : 20, margin:20}}
+        onPress={()=>navigation.navigate("KaKaoLogOut",{screen:"KaKaoLogOut"})}
+        >
+        <Text style={{fontSize:30, color : 'white' }}>카카오 로그아웃</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
+
+
+const styles = StyleSheet.create({
+  container : {
+    flex:1,
+    justifyContent :'center',
+    alignItems :'center'
+  },
+})
