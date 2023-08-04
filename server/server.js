36a5ec77f9a42a024d5f9c052bd7555e49776935
정영록 <s9285s@naver.com>
2023-08-05 01:36:17 +0900

    webSocketTest

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const port = 8082;
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -42,25 +46,43 @@ async function getAddressFromCoordinates(longitude, latitude) {
 }
 
 const defaultData = [
-    { id : 1, size:30 ,title:"가자미", latitude: 35.0968275, longitude: 128.8538282, src1 : "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800" },
-    { id : 2, size:50,title:"참돔", latitude: 35.0968275+0.001, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800" },
-    { id : 3, size:300 ,title:"고래", latitude: 35.0968275+0.002, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"},
-    { id : 4, size:200 ,title:"상어", latitude: 35.0968275+0.003, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"},
-    { id : 5, size:60 ,title:"돌돔", latitude: 35.0968275+0.004, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800" },
-    { id : 6, size:30 ,title:"우럭", latitude: 35.0968275+0.005, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800" },
-    { id : 7, size:35 ,title:"광어", latitude: 35.0968275+0.006, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800" },
-    { id : 8, size:25 ,title:"고등어", latitude: 35.0968275+0.007, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800" },
-    { id : 9, size:10 ,title:"불가사리", latitude: 35.0968275+0.008, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800" },
-    { id : 10,size:13 ,title:"해삼", latitude: 35.0968275+0.009, longitude: 128.8538282,src1 : "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800" },
+  { id : 1, size:30 ,title:"가자미", latitude: 35.0968275, longitude: 128.8538282, src1 : "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"       , address : '부산 녹산동 어딘가 50-3'},
+  { id : 2, size:50,title:"참돔", latitude: 35.0968275+0.001, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"     , address : '부산 녹산동 어딘가 50-3'},
+  { id : 3, size:300 ,title:"고래", latitude: 35.0968275+0.002, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"   , address : '부산 녹산동 어딘가 50-3'},
+  { id : 4, size:200 ,title:"상어", latitude: 35.0968275+0.003, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"   , address : '부산 녹산동 어딘가 50-3'},
+  { id : 5, size:60 ,title:"돌돔", latitude: 35.0968275+0.004, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"    , address : '부산 녹산동 어딘가 50-3'},
+  { id : 6, size:30 ,title:"우럭", latitude: 35.0968275+0.005, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"    , address : '부산 녹산동 어딘가 50-3'},
+  { id : 7, size:35 ,title:"광어", latitude: 35.0968275+0.006, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"    , address : '부산 녹산동 어딘가 50-3'},
+  { id : 8, size:25 ,title:"고등어", latitude: 35.0968275+0.007, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"  , address : '부산 녹산동 어딘가 50-3'},
+  { id : 9, size:10 ,title:"불가사리", latitude: 35.0968275+0.008, longitude: 128.8538282, src1: "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800", address : '부산 녹산동 어딘가 50-3' },
+  { id : 10,size:13 ,title:"해삼", latitude: 35.0968275+0.009, longitude: 128.8538282,src1 : "https://mblogthumb-phinf.pstatic.net/MjAyMTA2MDdfMjU5/MDAxNjIzMDcxOTIzNzEz.6dUa0UQGoXgrAqyPJtkbzU4oMn069zjD6qbNKwSlCsIg.VSexkq13-JCdjvGLbiv-vsawdrcrvG9dCq8_sAvzAOcg.JPEG.bestehno1/IMG_4495.jpg?type=w800"    , address : '부산 녹산동 어딘가 50-3'},
 ]
+
+const clients = []; // SSE 클라이언트를 저장할 배열
+io.on('connection', (socket) => {
+  console.log('클라이언트가 연결되었습니다.');
+
+  // 클라이언트를 저장합니다.
+  clients.push(socket);
+
+  // 클라이언트와의 연결이 끊어지면 배열에서 제거합니다.
+  socket.on('disconnect', () => {
+    console.log('클라이언트와의 연결이 끊어졌습니다.');
+    const index = clients.indexOf(socket);
+    if (index !== -1) {
+      clients.splice(index, 1);
+    }
+  });
+});
+
 app.post('/update', async (req, res) => {
   const newData = req.body;
   // console.log("Received data from client:", newData);
-
+  
   // 서버 데이터를 업데이트합니다.
-
+  
   console.log(data)
-    // API를 통해 주소 정보를 가져옵니다.
+  // API를 통해 주소 정보를 가져옵니다.
   try {
     const address = await getAddressFromCoordinates(newData.longitude, newData.latitude);
     data = {
@@ -72,7 +94,6 @@ app.post('/update', async (req, res) => {
       src1: newData.src1,
       address : address.address_name
 
-
     };
   } catch (error) {
     console.error("Error while getting address:", error.message);
@@ -81,14 +102,12 @@ app.post('/update', async (req, res) => {
   defaultData.push(data)
   // 연결된 모든 SSE 클라이언트에게 업데이트된 데이터를 전송합니다.
   clients.forEach((client) => {
-    data.timestamp = new Date().toISOString();
-    client.write(`data: ${JSON.stringify(data)}\n\n`);
+    client.emit('dataUpdated', defaultData);
   });
 
   res.status(200).json(defaultData);
 });
 
-const clients = []; // SSE 클라이언트를 저장할 배열
 
 app.get('/sse', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
@@ -107,7 +126,7 @@ app.get('/sse', (req, res) => {
   });
 });
 
-const server = app.listen(port, () => {
+server.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
 
