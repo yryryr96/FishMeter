5031d75a060142b3bdfba90abd5a3670fe38240f
junghyunwoo <wjdgusdndi@gmail.com>
2023-08-08 17:58:03 +0900

    feat: add address

---
+++ backend/src/main/java/com/example/fishdex/util/AddressInfo.java
@@ -0,0 +1,40 @@
+package com.example.fishdex.util;
+
+import lombok.Data;
+
+@Data
+public class AddressInfo {
+    private Document[] documents;
+
+    @Data
+    public static class Document {
+        private RoadAddress road_address;
+        private Address address;
+    }
+
+    @Data
+    public static class RoadAddress {
+        private String address_name;
+        private String region_1depth_name;
+        private String region_2depth_name;
+        private String region_3depth_name;
+        private String road_name;
+        private String underground_yn;
+        private String main_building_no;
+        private String sub_building_no;
+        private String building_name;
+        private String zone_no;
+    }
+
+    @Data
+    public static class Address {
+        private String address_name;
+        private String region_1depth_name;
+        private String region_2depth_name;
+        private String region_3depth_name;
+        private String mountain_yn;
+        private String main_address_no;
+        private String sub_address_no;
+    }
+}
+
