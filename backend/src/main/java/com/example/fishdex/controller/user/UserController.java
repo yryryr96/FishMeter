1f2775538e16b88d3e349611a4361dc6c936bf1d
junghyunwoo <wjdgusdndi@gmail.com>
2023-08-09 17:21:39 +0900

    feat: kakao login

--- backend/src/main/java/com/example/fishdex/controller/user/UserController.java
+++ backend/src/main/java/com/example/fishdex/controller/user/UserController.java
@@ -1,22 +1,31 @@
 package com.example.fishdex.controller.user;
 
-import com.example.fishdex.service.user.UserService;
+import com.example.fishdex.service.user.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+    private final CustomOAuth2UserService userService;
 
-    @GetMapping("/root")
+    @GetMapping("/user/root")
     @ResponseStatus(code = HttpStatus.OK, reason = "OK")
     public void isOk() {
+    }
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        System.out.println("principal.getAttributes() = " + principal.getAttributes());
+        return Collections.singletonMap("id", principal.getAttribute("id"));
     }
 }
