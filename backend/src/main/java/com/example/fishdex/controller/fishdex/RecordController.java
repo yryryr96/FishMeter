fe0fbee0696b66ad3065bf711ca624c936dc20c4
deerKBS <deer980119@gmail.com>
2023-08-13 18:05:38 +0900

    fix : 등록시 sse 에러 발생 문제 해결 - 객체를 json 형식으로 변환해 전송하도록 수정

--- backend/src/main/java/com/example/fishdex/controller/fishdex/RecordController.java
+++ backend/src/main/java/com/example/fishdex/controller/fishdex/RecordController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 //import org.springframework.security.core.annotation.AuthenticationPrincipal;
 //import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
